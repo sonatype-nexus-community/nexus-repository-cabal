2b013209d29bc0dae83f27f3c32e1d68ce7fccba
Jeffry Hesse <jhesse@sonatype.com>
2019-10-08 10:17:12 -0800

    Remove all whitespace

--- nexus-repository-cabal/src/main/java/org/sonatype/nexus/plugins/cabal/internal/util/CabalParser.java
+++ nexus-repository-cabal/src/main/java/org/sonatype/nexus/plugins/cabal/internal/util/CabalParser.java
@@ -52,7 +52,8 @@ public class CabalParser
 
   private void checkIfVersionOrName(final String scan, Map<String, Object> map) {
     if (scan.contains("name") || scan.contains("version")) {
-      String[] results = scan.split(": ");
+      String newLine = scan.replaceAll("\\s", "");
+      String[] results = newLine.split(":");
       map.put(results[0], results[1]);
     }
   }
